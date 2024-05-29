d7c45cebbfc0d53c2b7d1cb44f39fd85ea262657
akashyap25 <anuragkashyap026@gmail.com>
2024-05-29 21:42:21 +0530

    error console

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -27,7 +27,7 @@
 
           setEvents(response.data.events);
         } catch (error) {
-          console.error("Error fetching events:", error);
+          console.error("Error fetching events:", new Error(error));
         }
       };
       fetchEvents();
